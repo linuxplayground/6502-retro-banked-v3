2a267a160985287a513464c2035bff067372f7e5
David Latham <david.latham.nz@gmail.com>
2024-07-17 22:01:55 +1200

    VGM Player clean up

--- apps/vgm/main.s
+++ apps/vgm/main.s
@@ -15,26 +15,26 @@ vgmwaitl  = $F2
 vgmwaith  = $F3
 
 acia_putc = $FF09
-prbyte          = $FF0F
-primm           = $FF15
-sn_start        = $FF6C
-sn_stop         = $FF6F
-sn_silence      = $FF72
-sn_send         = $FF84
+prbyte    = $FF0F
+primm     = $FF15
+sn_start  = $FF6C
+sn_stop   = $FF6F
+sn_silence= $FF72
+sn_send   = $FF84
 
 .code
     jsr sn_start
     jsr sn_silence
     jsr vgm_setup
-    ldy #$80
+    ldy #$80        ; all samples I have seen begin at 0x80 in the file.
     jsr vgm_play
     jsr sn_stop
     rts
 
 ; sets up the VGM data stream
 vgm_setup:
-    lda #1
-    sta ram_bank
+    lda #1          ; this player expects the song file to have been loaded
+    sta ram_bank    ; into extended memory starting in BANK 1 at 0xA000
     sta rambankreg
     ; assign pointers
     stz vgmptr
@@ -80,32 +80,32 @@ vgm_play:
 :   rts
 
 vgm_next:
-    iny
-    bne :+
+    iny                 ; increment the y index into the data pointed to by
+    bne :+              ; vgmptr and ensure that ram bank boundaries are managed
     jsr incvgmptrh
 :   jmp vgm_play
 
 incvgmptrh:
-    lda #'.'
+    lda #'.'            ; print a '.' every 256 bytes
     jsr acia_putc
 
     inc vgmptr + 1
     lda vgmptr + 1
-    cmp #$C0
-    bne :+
+    cmp #$C0            ; have we crossed into ROM?
+    bne :+              ; no - return
 
-    inc ram_bank
+    inc ram_bank        ; move to next ram bank.
     lda ram_bank
     sta rambankreg
-    nop
-    nop
+    nop                 ; the 74LS273 registers I am using appear to
+    nop                 ; need these extra cycles.
 
-    lda ram_bank
+    lda ram_bank        ; show the new rambank to the user.
     jsr prbyte
 
-    lda #$A0
-    sta vgmptr + 1
-    ldy #0
+    lda #$A0            ; reset the vgmptr to the start of the ram
+    sta vgmptr + 1      ; bank
+    ldy #0              ; reset y to 0.
 :   rts
 
 command:
@@ -116,9 +116,9 @@ command:
     jsr sn_send
     jmp vgm_next
 
-wait:
-    iny
-    bne :+
+wait:                   ; get the next two bytes taking care to account
+    iny                 ; for crossing to the next ram bank.  These form
+    bne :+              ; the 16 bit wide number of samples to wait for.
     jsr incvgmptrh
 :   lda (vgmptr),y
     sta vgmwaitl
@@ -126,12 +126,12 @@ wait:
     bne :+
     jsr incvgmptrh
 :   lda (vgmptr),y
-    sta vgmwaith
-    jsr vgmwait
+    sta vgmwaith        ; once the vgmwait word has the number of samples
+    jsr vgmwait         ; to wait for, go ahead and perform the wait.
     jmp vgm_next
 
-n1:
-    lda (vgmptr),y
+n1:                     ; this special case, meand wait for up to 15 sample
+    lda (vgmptr),y      ; periods
     cmp #$70
     beq :+
     and #$0f
@@ -141,22 +141,29 @@ n1:
 :
     jmp vgm_next
 
-end:
+end:                    ; reset the rambank and silence the PSG
     stz ram_bank
     stz rambankreg
     jmp sn_silence
 
-sixtieth:
-    lda #$df
+sixtieth:               ; as given by the datasheet wait for exactly 1/60
+    lda #$df            ; of a second.
     sta vgmwaitl
     lda #$02
     sta vgmwaith
     jsr vgmwait
     jmp vgm_next
 
+; this routine waste a fairly large number of bytes.  As this is the ONLY program
+; being executed from Low memory when it's running, there is plenty of space for
+; inefficient yet fast code.  A bunch-o-nops is a perfectly reasonable way to kill
+; precises amounts of time in 6502 Assembly if you have the room.  Certainly it can
+; be done with VIA timers or clever loops, but with the ram available this seems to
+; work just fine.
+;
 ; at 4mhz we want 91 clocks for a single sample.
 ; this works out to 90 clock cycles. which is ~22uS
-vgmwait:                    ; (6) Cycles to prep the and execute the jsr
+vgmwait:                    ; (6) Cycles to prep and execute the jsr
     lda vgmwaitl            ; (3)
     bne @wait_samples_1     ; (2)   (could be 3 if branching across page)
     lda vgmwaith            ; (3)
@@ -171,6 +178,4 @@ vgmwait:                    ; (6) Cycles to prep the and execute the jsr
     jmp vgmwait             ; (3)   loop = 29 cycles
 @return:
     rts                     ; (6)   6 cycles to return
-
-vgm_start:  .dword 0
 
